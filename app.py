88176cb45e355f7258795152694fc2ac814def37
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-14 12:27:21 -0400

    Add beginning app functions

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
 from model import *
 
-def greet():
-	print('Hiya!')
-	return 'Hiya'
+# import click
+# # https://click.palletsprojects.com/en/7.x/quickstart/
 
-greet()
+# @click.command()
+# def hiya():
+# 	click.echo('Hiya luna!')
+
+# @click.command()
+# def main():
+# 	"""
+# 	This is a flash card project to help you with multiplication
+# 	"""
+# 	print(f'Flash cards')
+
+# if __name__ == '__main__':
+# 	# hiya()
+# 	main()
+
+
+def get_cards(num_cards):
+	all_cards = Card.select().where(Card.id < int(num_cards) + 1)
+	for card in all_cards:
+		print(card.question)
+
+def begin_session():
+	num_cards = input(f'How many flash cards? Enter number between 1 and 36: ')
+	print(f'Retrieving {num_cards} flash cards')
+	get_cards(num_cards)
+
+def main():
+	# welcome_msg = 'Welcome to Maths-CLI. This is a flash card project to help you practice multiplication.'
+	# set_up ='Set up: Install dependencies in the Pipfile with pipenv. Then enter "pipenv shell" in the terminal to create a virtual environment.'
+	# create_dbase = 'Create Database: Now open a separate terminal window enter "psql". At the =# prommpt, enter "CREATE DATABASE maths;"'
+	# seed_dbase = 'Seed Database: Back in the virtual environment terminal, enter "python3 seed.py"'
+	# print(welcome_msg)
+	# print(set_up)
+	# print(create_dbase)
+	# print(seed_dbase)
+	begin_session()
+
+main()
