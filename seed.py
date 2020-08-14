a675bdcb5fcccce402814bdc397ffcccac0ff383
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-14 09:58:37 -0400

    Comment out seeding function

--- seed.py
+++ seed.py
@@ -9,8 +9,8 @@ for i in range(0, greatestFactor*greatestFactor+1):
 	cards.append(f'card{i}')
 
 # Seed dbase with cards model
-for i in range(1, greatestFactor+1):
-	for j in range(1, greatestFactor+1):
-		index+= 1
-		cards[index] = Card(series=i, question=f"{i} x {j}", answer=i*j, correct=0, incorrect=0)
-		cards[index].save()
+# for i in range(1, greatestFactor+1):
+# 	for j in range(1, greatestFactor+1):
+# 		index+= 1
+# 		cards[index] = Card(series=i, question=f"{i} x {j}", answer=i*j, correct=0, incorrect=0)
+# 		cards[index].save()
