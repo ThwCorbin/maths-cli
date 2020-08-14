185887ead5c8be385948eae9e60c389547eac6de
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-14 09:42:39 -0400

    Change largest to greatest

--- seed.py
+++ seed.py
@@ -1,16 +1,16 @@
 from model import *
 
-largestFactor = 6
+greatestFactor = 6
 cards = []
 index = 0
 
 # Create card variable names
-for i in range(0, largestFactor*largestFactor+1):
+for i in range(0, greatestFactor*greatestFactor+1):
 	cards.append(f'card{i}')
 
 # Seed dbase with cards model
-for i in range(1, largestFactor+1):
-	for j in range(1, largestFactor+1):
+for i in range(1, greatestFactor+1):
+	for j in range(1, greatestFactor+1):
 		index+= 1
 		cards[index] = Card(series=i, question=f"{i} x {j}", answer=i*j, correct=0, incorrect=0)
 		cards[index].save()
