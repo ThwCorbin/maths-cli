be9c9e3b2484e65b1fd52239d4be55ce3866337a
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-14 09:30:10 -0400

    Write seed and implement

--- seed.py
+++ seed.py
@@ -1,2 +1,14 @@
 from model import *
 
+largestFactor = 6
+cards = []
+index = 0
+
+for i in range(0, largestFactor*largestFactor+1):
+	cards.append(f'card{i}')
+
+for i in range(1, largestFactor+1):
+	for j in range(1, largestFactor+1):
+		index+= 1
+		cards[index] = Card(series=i, question=f"{i} x {j}", answer=i*j, correct=0, incorrect=0)
+		cards[index].save()
